a3f4ad8a367e963271eb00b7d84cf5d6e378dd4a
GhazaFadhlilbaqi <fadhlilbaqimuhammadghaza@gmail.com>
2024-11-19 18:35:28 +0700

    Did Assignment 9 (django part)

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ from main.views import show_xml, show_json, show_xml_by_id, show_json_by_id
 from main.views import register, login_user, logout_user
 from main.views import edit_product, delete_product
 from main.views import add_product_entry_ajax
+from main.views import create_product_entry_flutter
 
 app_name = 'main'
 
@@ -20,4 +21,5 @@ urlpatterns = [
     path('edit_product/<uuid:id>', edit_product, name='edit_product'),
     path('delete/<uuid:id>', delete_product, name='delete_product'),
     path('add-product-entry-ajax', add_product_entry_ajax, name='add_product_entry_ajax'),
+    path('create-flutter/', create_product_entry_flutter, name='create_product_entry_flutter'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse 
 from main.forms import UnlimitedBaconForm
 from main.models import UnlimitedBacon
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.core import serializers
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
@@ -12,6 +12,7 @@ from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
+import json
 
 @login_required(login_url='/login')
 def show_main(request):
@@ -132,3 +133,22 @@ def add_product_entry_ajax(request):
     new_product.save()
 
     return HttpResponse(b"CREATED", status=201)
+
+@csrf_exempt
+def create_product_entry_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_product = UnlimitedBacon.objects.create(
+            user=request.user,
+            name=data["name"],
+            price=int(data["price"]),
+            description=data["description"],
+            stock=int(data["stock"])
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401) 
